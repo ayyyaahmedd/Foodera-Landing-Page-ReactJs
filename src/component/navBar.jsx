d97a4ab634940c06d25f1d2423bc5d62423f7a36
Aya <ayaelrefaay580@gmail.com>
2023-12-16 06:16:57 +0200

    fix-routing

--- src/component/navBar.jsx
+++ src/component/navBar.jsx
@@ -1,31 +1,47 @@
 import React from 'react';
-import './NavStyle.css'
-import logo from './images/logo.png'
+import { LinkContainer } from 'react-router-bootstrap';
+import './NavStyle.css';
+import logo from './images/logo.png';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-
 const NavBar = () => {
-    return ( 
-        <>
-        <Navbar expand="lg" className="bg-body-tertiary">
-          <Container>
-            <Navbar.Brand href="#home" className="navbar-brand"><img src={logo} alt="logo" title='logo' /></Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav  className="navbar-nav  ms-auto mb-2 mb-lg-0">
-                <Nav.Link className="nav-link active list-item" aria-current="page" href="/">Home</Nav.Link>
-                <Nav.Link className="nav-link list-item" href="/about">About</Nav.Link>
-                <Nav.Link className="nav-link list-item" href="#">Explore Food</Nav.Link>
-                <Nav.Link className="nav-link list-item" href="#">Reviews</Nav.Link>
-                <Nav.Link className="nav-link contact-num" href="#">+111 222 333</Nav.Link>
-              </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-        </>
-    );
-}
+  return (
+    <>
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand className="navbar-brand">
+              <img src={logo} alt="logo" title="logo" />
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <LinkContainer to="/" exact>
+                <Nav.Link className="nav-link active list-item" aria-current="page">
+                  Home
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/about">
+                <Nav.Link className="nav-link list-item">About</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/explore-food">
+                <Nav.Link className="nav-link list-item">Explore Food</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/reviews">
+                <Nav.Link className="nav-link list-item">Reviews</Nav.Link>
+              </LinkContainer>
+              <Nav.Link className="nav-link contact-num" href="#">
+                +111 222 333
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
 
 export default NavBar;
